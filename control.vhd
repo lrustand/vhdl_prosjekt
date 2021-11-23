450eff36dec56e2a688f419902a5d8172a675ff3
Lars Rustand <rustand.lars@gmail.com>
2021-11-23 11:03:20 +0100

    Add state 0

--- control.vhd
+++ control.vhd
@@ -4,6 +4,7 @@ use IEEE.std_logic_1164.all;
 
 entity control_path is
     port (
+        clk : in std_logic;
         input_char : in std_logic;
         rx_done : in std_logic;
         tx_done : in std_logic;
@@ -29,3 +30,25 @@ entity control_path is
 end entity control_path;
 
 
+architecture arch of control_path is
+    variable state : std_logic_vector(2 downto 0) := 0;
+begin
+    process
+    begin
+        if state = 0 then
+            wheel_roms_cnt_inc <= clk;
+            if wheel_roms_cnt > 25 then
+                state := 1;
+            else
+                load_wheels <= clk;
+            end if;
+        elsif state = 1 then
+
+        elsif state = 2 then
+        elsif state = 3 then
+        elsif state = 4 then
+        elsif state = 5 then
+        elsif state = 6 then
+        end if;
+    end process;
+end arch;
