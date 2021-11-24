7a0f96e951289fe3e1a9eda80d4b5113852e77bf
Nelde95 <59565192+Nelde95@users.noreply.github.com>
2021-11-24 10:53:52 +0100

    exposed index by moving it to output

--- rotor.vhd
+++ rotor.vhd
@@ -5,15 +5,14 @@ use IEEE.std_logic_unsigned.all;
 
 entity rotor is
     -- Size of vectors will be automatically assigned based on external port mapping, comments bellow are the expected values
-    port(rom_data1, rom_data2, addr_in1, addr_in2: in std_logic_vector(4 downto 0);
-                                     rst, we, inc: in std_logic;
-       rom_addr1, rom_addr2, addr_out1, addr_out2: out std_logic_vector(4 downto 0)
+    port(        rom_data1, rom_data2, addr_in1, addr_in2: in std_logic_vector(4 downto 0);
+                                             rst, we, inc: in std_logic;
+        rom_addr1, rom_addr2, addr_out1, addr_out2, index: out std_logic_vector(4 downto 0)
     );
 end rotor;
 
 architecture Behavioral of rotor is
     constant LETTERS: integer := 26;
-    signal index: std_logic_vector(addr_in1'length-1 downto 0);
     signal add_mod_ext1a, add_mod_ext1b, add_mod_ext2a, add_mod_ext2b: std_logic_vector(addr_in1'length downto 0);
 begin
     process(rst, inc) begin
