c5645b43fe32855f6373790095854da190e8d82d
Nelde95 <Nelde95@yahoo.com>
2021-11-24 09:38:02 +0100

    changed rotor.vhd to use rom directly

--- rotor.vhd
+++ rotor.vhd
@@ -5,27 +5,17 @@ use IEEE.std_logic_unsigned.all;
 
 entity rotor is
     -- Size of vectors will be automatically assigned based on external port mapping, comments bellow are the expected values
-    port(rom_addr, rom_data, addr_in1, addr_in2: in std_logic_vector(4 downto 0); -- should be 4 downto 0
-                                   rst, we, inc: in std_logic;
-                           addr_out1, addr_out2: out std_logic_vector(4 downto 0) -- should be same as above
+    port(rom_data1, rom_data2, addr_in1, addr_in2: in std_logic_vector(4 downto 0);
+                                     rst, we, inc: in std_logic;
+       rom_addr1, rom_addr2, addr_out1, addr_out2: out std_logic_vector(4 downto 0)
     );
 end rotor;
 
 architecture Behavioral of rotor is
     constant LETTERS: integer := 26;
-    type ram_type is array(LETTERS-1 downto 0)
-        of std_logic_vector(rom_addr'length-1 downto 0);
-    signal ram1, ram2: ram_type;
-    signal index, downscale1, downscale2: std_logic_vector(rom_addr'length-1 downto 0);
-    signal add_mod_ext1a, add_mod_ext1b, add_mod_ext2a, add_mod_ext2b: std_logic_vector(rom_addr'length downto 0);
+    signal index, downscale1, downscale2: std_logic_vector(addr_in1'length-1 downto 0);
+    signal add_mod_ext1a, add_mod_ext1b, add_mod_ext2a, add_mod_ext2b: std_logic_vector(addr_in1'length downto 0);
 begin
-    process(we) begin
-        if(we'event and we='1') then
-            ram1(to_integer(unsigned(rom_addr))) <= rom_data;
-            ram2(to_integer(unsigned(rom_data))) <= rom_addr;
-        end if;
-    end process;
-
     process(rst, inc) begin
         if(rst='1') then
             index <= "00000";
@@ -35,12 +25,12 @@ begin
     end process;
 
     add_mod_ext1a <= std_logic_vector(unsigned('0'&addr_in1 + index) mod LETTERS);
-    downscale1 <= add_mod_ext1a(4 downto 0);
-    add_mod_ext1b <= std_logic_vector(unsigned('0'&ram1(to_integer(unsigned(downscale1))) + LETTERS - index) mod LETTERS);
+    rom_addr1 <= add_mod_ext1a(4 downto 0);
+    add_mod_ext1b <= std_logic_vector(unsigned('0'&rom_data1 + LETTERS - index) mod LETTERS);
     addr_out1 <= add_mod_ext1b(4 downto 0);
     
     add_mod_ext2a <= std_logic_vector(unsigned('0'&addr_in2 + index) mod LETTERS);
-    downscale2 <= add_mod_ext2a(4 downto 0);
-    add_mod_ext2b <= std_logic_vector(unsigned('0'&ram2(to_integer(unsigned(downscale2))) + LETTERS - index) mod LETTERS);
+    rom_addr2 <= add_mod_ext2a(4 downto 0);
+    add_mod_ext2b <= std_logic_vector(unsigned('0'&rom_data2 + LETTERS - index) mod LETTERS);
     addr_out2 <= add_mod_ext2b(4 downto 0);
 end Behavioral;
