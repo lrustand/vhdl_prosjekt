eef774aa282d4cb330c89c4bdf02b3a015578bee
Nelde95 <Nelde95@yahoo.com>
2021-11-24 13:32:51 +0100

    better names and indentation

--- rotor.vhd
+++ rotor.vhd
@@ -5,15 +5,15 @@ use IEEE.std_logic_unsigned.all;
 
 entity rotor is
     -- Size of vectors will be automatically assigned based on external port mapping, comments bellow are the expected values
-    port(        rom_data1, rom_data2, addr_in1, addr_in2: in std_logic_vector(4 downto 0);
-                                                 rst, inc: in std_logic;
+    port(rst, inc: in std_logic;
+        rom_data1, rom_data2, addr_in1, addr_in2: in std_logic_vector(4 downto 0);
         rom_addr1, rom_addr2, addr_out1, addr_out2, index: out std_logic_vector(4 downto 0)
     );
 end rotor;
 
 architecture Behavioral of rotor is
     constant LETTERS: integer := 26;
-    signal add_mod_ext1a, add_mod_ext1b, add_mod_ext2a, add_mod_ext2b: std_logic_vector(addr_in1'length downto 0);
+    signal rotate1, rotate_back1, rotate2, rotate_back2: std_logic_vector(addr_in1'length downto 0);
 begin
     process(rst, inc) begin
         if(rst='1') then
@@ -23,13 +23,13 @@ begin
         end if;
     end process;
 
-    add_mod_ext1a <= std_logic_vector(unsigned('0'&addr_in1 + index) mod LETTERS);
-    rom_addr1 <= add_mod_ext1a(4 downto 0);
-    add_mod_ext1b <= std_logic_vector(unsigned('0'&rom_data1 + LETTERS - index) mod LETTERS);
-    addr_out1 <= add_mod_ext1b(4 downto 0);
-    
-    add_mod_ext2a <= std_logic_vector(unsigned('0'&addr_in2 + index) mod LETTERS);
-    rom_addr2 <= add_mod_ext2a(4 downto 0);
-    add_mod_ext2b <= std_logic_vector(unsigned('0'&rom_data2 + LETTERS - index) mod LETTERS);
-    addr_out2 <= add_mod_ext2b(4 downto 0);
+    rotate1 <= std_logic_vector(unsigned('0'&addr_in1 + index) mod LETTERS);
+    rom_addr1 <= rotate1(4 downto 0);
+    rotate_back1 <= std_logic_vector(unsigned('0'&rom_data1 + LETTERS - index) mod LETTERS);
+    addr_out1 <= rotate_back1(4 downto 0);
+
+    rotate2 <= std_logic_vector(unsigned('0'&addr_in2 + index) mod LETTERS);
+    rom_addr2 <= rotate2(4 downto 0);
+    rotate_back2 <= std_logic_vector(unsigned('0'&rom_data2 + LETTERS - index) mod LETTERS);
+    addr_out2 <= rotate_back2(4 downto 0);
 end Behavioral;
