c2f9359071a05cae9db8fd372e9b047dacff2b6c
Ola <os@hotmail.no>
2021-11-26 08:42:59 +0100

    removed work-object and fixed hex numbering

--- ascii_to_five_bit.vhd
+++ ascii_to_five_bit.vhd
@@ -13,7 +13,7 @@ architecture Behavioral of ascii_to_five_bit is
 begin
     process(clk)
     begin
-        if ascii_in(6 downto 5) >= x"2" and ascii_in(4 downto 0) >= x"1" and ascii_in(4 downto 0) < x"1b" then
+        if ascii_in(7 downto 0) > x"40" and ascii_in(4 downto 0) < x"5b" or ascii_in(7 downto 0) > x"60" and ascii_in(7 downto 0) < x"7b"then
             converted_ascii <= std_logic_vector( unsigned(ascii_in(4 downto 0)) - "1" );
         elsif ascii_in = x"0d" then
             converted_ascii <= ascii_in(4 downto 0);
