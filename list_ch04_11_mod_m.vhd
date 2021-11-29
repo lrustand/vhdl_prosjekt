ead65b5b7bd1e3bebe334d7b66cebd0fe458bf9d
Lars Rustand <rustand.lars@gmail.com>
2021-11-29 08:57:38 +0100

    Remove q

--- list_ch04_11_mod_m.vhd
+++ list_ch04_11_mod_m.vhd
@@ -10,7 +10,6 @@ entity mod_m_counter is
    port(
       clk, reset: in std_logic;
       max_tick: out std_logic;
-      q: out std_logic_vector(N-1 downto 0)
    );
 end mod_m_counter;
 
@@ -31,6 +30,5 @@ begin
    r_next <= (others=>'0') when r_reg=(M-1) else
              r_reg + 1;
    -- output logic
-   q <= std_logic_vector(r_reg);
    max_tick <= '1' when r_reg=(M-1) else '0';
 end arch;
