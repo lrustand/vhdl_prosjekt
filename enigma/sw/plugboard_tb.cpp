d82aa4e445fa77ac9940274c7a8bd0e766a570d6
Lars Rustand <rustand.lars@gmail.com>
2021-12-03 09:40:39 +0100

    Add summary to plugboard_tb

--- enigma/sw/plugboard_tb.cpp
+++ enigma/sw/plugboard_tb.cpp
@@ -3,17 +3,25 @@
 
 // Test that the path through the plugboard is the same both ways,
 // e.g. if A => F, then F => A should also be true
-void test_plugboard_consistency()
+bool test_plugboard_consistency()
 {
+    bool success = true;
     for (const auto& [key, value] : plugboard) {
         if (key != plugboard[value]) {
             printf("ERROR: %c = %c, but %c != %c. ", key, value, value, key);
             printf("Actual %c = %c\n", value, plugboard[value]);
+            success = false;
         }
     }
+    return success;
 }
 
 int main()
 {
-    test_plugboard_consistency();
+    if (test_plugboard_consistency()) {
+        printf("plugboard_tb passed all tests.\n");
+    }
+    else {
+        printf("plugboard_tb failed 1 test(s).\n");
+    }
 }
