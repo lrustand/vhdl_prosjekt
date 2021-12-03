983173f7a18b1ad2c570fcb84c30669f23d49f81
Lars Rustand <rustand.lars@gmail.com>
2021-12-03 09:46:54 +0100

    Add summary to reflector_tb

--- enigma/sw/reflector_tb.cpp
+++ enigma/sw/reflector_tb.cpp
@@ -3,37 +3,53 @@
 
 // Test that the path through the reflector is the same both ways,
 // e.g. if A => Y, then Y => A should also be true
-void test_reflector_consistency()
+bool test_reflector_consistency()
 {
+    bool success = true;
     for (const auto& [key, value] : reflector) {
         if (key != reflector[value]) {
             printf("ERROR: %c = %c, but %c != %c. ", key, value, value, key);
             printf("Actual %c = %c\n", value, reflector[value]);
+            success = false;
         }
     }
+    return success;
 }
 
 // Helper function for verifying expected char mapping
-void _verify(char key, char expected_value)
+bool _verify(char key, char expected_value)
 {
+    bool success = true;
     char actual_value = reflector[key];
-    if (actual_value != expected_value)
+    if (actual_value != expected_value) {
         printf("ERROR: %c != %c, but is instead %c\n", key, expected_value, actual_value);
+        success = false;
+    }
+    return success;
 }
 
 // Tests a few random chars to see if they match the mapping of the real reflector
-void test_reflector_correct_mapping()
+bool test_reflector_correct_mapping()
 {
-
-    _verify('A', 'Y');
-    _verify('R', 'B');
-    _verify('V', 'W');
-    _verify('C', 'U');
+    return
+        _verify('A', 'Y') &&
+        _verify('R', 'B') &&
+        _verify('V', 'W') &&
+        _verify('C', 'U');
 }
 
 
 int main()
 {
-    test_reflector_consistency();
-    test_reflector_correct_mapping();
+    int failed_testcases = 0;
+
+    failed_testcases += !test_reflector_consistency();
+    failed_testcases += !test_reflector_correct_mapping();
+
+    if (failed_testcases) {
+        printf("reflector_tb failed %d test(s).\n");
+    }
+    else {
+        printf("reflector_tb passed all tests.\n");
+    }
 }
