67f321ce3c517630f19adaf47dd9d612602f9dbf
Lars Rustand <rustand.lars@gmail.com>
2021-12-03 09:33:33 +0100

    Add summary to converter_tb

--- enigma/sw/ascii_converter_tb.cpp
+++ enigma/sw/ascii_converter_tb.cpp
@@ -3,48 +3,82 @@
 
 using namespace ascii_converter;
 
-void test_to_ascii(int i, char expected)
+// Test int to ascii conversion
+bool test_to_ascii(int i, char expected)
 {
+    bool success = true;
     char actual = to_ascii(i);
     if (actual != expected) {
         printf("ERROR: %d != %c. Actual %d = %c.\n", i, expected, i, actual);
+        success = false;
     }
+    return success;
 }
 
-void test_from_ascii(char c, int expected)
+// Test char to int conversion
+bool test_from_ascii(char c, int expected)
 {
+    bool success = true;
     char actual = from_ascii(c);
     if (actual != expected) {
         printf("ERROR: %c != %d. Actual %c = %d.\n", c, expected, c, actual);
+        success = false;
     }
+    return success;
 }
 
 // Test double conversion
-void test_conversion_consistency()
+bool test_conversion_consistency()
 {
+    bool success = true;
     for (int i=0; i<26; i++) {
         int converted = from_ascii(to_ascii(i));
-        if (converted != i)
+        if (converted != i) {
             printf("ERROR: %d != %d. Actual %d => %d.\n", i, i, i, converted);
+            success = false;
+        }
     }
 
     for (char c='A'; c<='Z'; c++) {
         char converted = to_ascii(from_ascii(c));
-        if (converted != c)
+        if (converted != c) {
             printf("ERROR: %c != %c. Actual %c => %c.\n", c, c, c, converted);
+            success = false;
+        }
     }
+    return success;
 }
 
 
 int main()
 {
-    test_to_ascii(0, 'A');
-    test_to_ascii(3, 'D');
-    test_to_ascii(25, 'Z');
+    int failed_testcases = 0;
 
-    test_from_ascii('B', 1);
-    test_from_ascii('F', 5);
-    test_from_ascii('Y', 24);
+    // Test a few random int to char conversions
+    if (!test_to_ascii(0, 'A') ||
+        !test_to_ascii(3, 'D') ||
+        !test_to_ascii(25, 'Z'))
+    {
 
-    test_conversion_consistency();
+        failed_testcases++;
+    }
+
+    // Test a few random char to int conversions
+    if (!test_from_ascii('B', 1) ||
+        !test_from_ascii('F', 5) ||
+        !test_from_ascii('Y', 24))
+    {
+
+        failed_testcases++;
+    }
+
+    if (!test_conversion_consistency()) failed_testcases++;
+
+
+    if (failed_testcases) {
+        printf("converter_tb failed %d test(s).\n");
+    }
+    else {
+        printf("converter_tb passed all tests.\n");
+    }
 }
