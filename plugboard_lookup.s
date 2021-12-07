56aa38cf2fcad2742215cbe263b97f5e2ddee238
lrustand <rustand.lars@gmail.com>
2021-12-07 10:25:13 +0100

    Add reflector an plugboard lookup assembly code

---
+++ plugboard_lookup.s
@@ -0,0 +1,12 @@
+pushq	%rbp
+movq	%rsp, %rbp
+movl	%edi, -4(%rbp)
+movl	-4(%rbp), %eax
+cltq
+leaq	0(,%rax,4), %rdx
+leaq	plugboard(%rip), %rax
+movl	(%rdx,%rax), %eax
+popq	%rbp
+ret
+
+# 9 instructions plus ret
